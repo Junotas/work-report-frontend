a6333384b84b1cf496b7d00f800df0e55b25111c
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-07-16 19:44:13 +0200

    changed font on profile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const Header: React.FC = () => {
   return (
     <header className="flex justify-between items-center p-4 bg-gray-800 text-white">
-      <Link to="/" className="text-xl font-bold">Home</Link>
-      <Link to="/profile" className="text-lg">Profile</Link>
+      <Link to="/" className="text-xl font-bold">Employee List</Link>
+      <Link to="/profile" className="text-xl font-bold">Profile</Link>
     </header>
   );
 };
